dd6e2990f2b48971455e6de35c6446c4b1aeda3e
Ayush Monga <121785988+AyushMonga@users.noreply.github.com>
2025-07-03 12:20:20 +0530

    Time: 22 ms (20.91%), Space: 26.4 MB (13.3%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,18 +1,20 @@
+#include <queue>
+
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int> circle;
+        std::queue<int> q;
         for (int i = 1; i <= n; ++i) {
-            circle.push_back(i);
+            q.push(i);
         }
-        int cur_ind = 0;
-
-        while (circle.size() > 1) {
-            int next_to_remove = (cur_ind + k - 1) % circle.size();
-            circle.erase(circle.begin() + next_to_remove);
-            cur_ind = next_to_remove;
+        while (q.size() > 1) {
+            for (int i = 1; i < k; ++i) {
+                int front = q.front();
+                q.pop();
+                q.push(front);
+            }
+            q.pop();
         }
-
-        return circle[0];
+        return q.front();
     }
 };
