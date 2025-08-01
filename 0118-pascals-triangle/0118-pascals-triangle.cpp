f0d9bd9cbf3bb87a5407502b5defe6a386e0f7e3
Ayush Monga <121785988+AyushMonga@users.noreply.github.com>
2025-08-02 02:08:38 +0530

    Time: 0 ms (100%), Space: 9.6 MB (88.32%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,25 +1,26 @@
 class Solution {
-public:
-    vector<int> generateRow(int row) {
-    long long ans = 1;
-    vector<int> ansRow;
-    ansRow.push_back(1); 
-    for (int col = 1; col < row; col++) {
-        ans = ans * (row - col);
-        ans = ans / col;
-        ansRow.push_back(ans);
-    }
-    return ansRow;
-}
+    public:
+        vector<int> generateRow(int row) {
+            long long ans = 1;
+                vector<int> ansRow;
+                    ansRow.push_back(1); 
+                        for (int col = 1; col < row; col++) {
+                                ans = ans * (row - col);
+                                        ans = ans / col;
+                                                ansRow.push_back(ans);
+                                                    }
+                                                        return ansRow;
+                                                        }
 
-vector<vector<int>> generate(int n) {
-    vector<vector<int>> ans;
+                                                        vector<vector<int>> generate(int n) {
+                                                            vector<vector<int>> ans;
 
-    for (int row = 1; row <= n; row++) {
-        ans.push_back(generateRow(row));
-    }
-    return ans;
-}
+                                                                for (int row = 1; row <= n; row++) {
+                                                                        ans.push_back(generateRow(row));
+                                                                            }
+                                                                                return ans;
+                                                                                }
 
 
-};
+                                                                                };
+
