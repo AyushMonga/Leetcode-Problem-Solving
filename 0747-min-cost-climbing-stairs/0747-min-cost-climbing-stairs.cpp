6d47b7a3bc341bb811b4f3b35278795bd27f0d15
Ayush Monga <121785988+AyushMonga@users.noreply.github.com>
2025-08-14 16:07:21 +0530

    Time: 0 ms (100%), Space: 17.8 MB (12.16%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n=cost.size();
-        vector<int>dp(n+1);
-        dp[0]=0;
-        dp[1]=0;
-        for(int i=2;i<=n;i++){
-            int option1=dp[i-1]+cost[i-1];
-            int option2=dp[i-2]+cost[i-2];
-            dp[i]=min(option1,option2);
+    // int helper(int idx,vector<int>&cost){
+    //     if(idx>=cost.size()){
+    //         return 0;
+    //     }    
+    //     int left=helper(idx+1,cost)+cost[idx];
+    //     int right=helper(idx+2,cost)+cost[idx];
+    //     return min(left,right);
+    // }
+    // int minCostClimbingStairs(vector<int>& cost) {
+    //     int a=helper(0,cost);
+    //     int b=helper(1,cost);
+    //     return min(a,b);
+    // }
+    //recursion TLE;
+
+    int helper(int idx,vector<int>&cost,vector<int>&dp){
+        if(idx>=cost.size()){
+            return 0;
         }
-        return dp[n];
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        int left=helper(idx+1,cost,dp)+cost[idx];
+        int right=helper(idx+2,cost,dp)+cost[idx];
+        dp[idx]=min(left,right);
+        return dp[idx];
+    }
+    int minCostClimbingStairs(vector<int>&cost){
+        vector<int>dp(cost.size(),-1);
+        int a=helper(0,cost,dp);
+        int b=helper(1,cost,dp);
+        return min(a,b);
     }
 };
