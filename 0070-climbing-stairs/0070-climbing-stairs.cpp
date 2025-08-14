363d156ece2b539160af30d71b83298aee49b59a
Ayush Monga <121785988+AyushMonga@users.noreply.github.com>
2025-08-14 11:44:18 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,11 +1,32 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        vector<int>ways(n+1);
-        ways[0]=ways[1]=1;
+    // int climbStairs(int n) {
+    //     if(n==0 || n==1)return 1;
+    //     return climbStairs(n-1)+climbStairs(n-2);
+    // }
+    // Recursion TLE:
+
+
+
+    // int climbStairs(int n){
+    //     vector<int>dp(n+1,-1);
+    //     if(n==0||n==1){
+    //         return 1;
+    //     }
+    //     if(dp[n]==-1){
+    //         dp[n]=climbStairs(n-1)+climbStairs(n-2);
+    //     }
+    //     return dp[n];
+    // }
+    // memoization MLE;
+
+    int climbStairs(int n){
+        vector<int>dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
         for(int i=2;i<=n;i++){
-            ways[i]=ways[i-1]+ways[i-2];
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return ways[n];
+        return dp[n];
     }
 };
