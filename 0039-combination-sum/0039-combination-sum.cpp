57341f4522858abf4fef72dba3d7ffa8b1a3a9e3
Ayush Monga <121785988+AyushMonga@users.noreply.github.com>
2025-08-15 22:12:06 +0530

    Time: 11 ms (27.08%), Space: 18.8 MB (24.84%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    void helper(int idx, int target,vector<int>&sol,vector<vector<int>>&ans,vector<int>&candidates){
+    void helper(int idx,int target,vector<int>&candidates,vector<int>&path,vector<vector<int>>&ans){
+        if(target==0){
+            ans.push_back({path});
+            return;
+        }
         if(idx==candidates.size() || target<0){
             return;
         }
-        if(target==0){
-            ans.push_back({sol});
-            return;
-        }
-        sol.push_back(candidates[idx]);
-        helper(idx+1,target-candidates[idx],sol,ans,candidates);
-        helper(idx,target-candidates[idx],sol,ans,candidates);
-        sol.pop_back();
-        helper(idx+1,target,sol,ans,candidates);
+        path.push_back(candidates[idx]);
+        helper(idx+1,target-candidates[idx],candidates,path,ans);
+        helper(idx,target-candidates[idx],candidates,path,ans);
+        path.pop_back();
+        helper(idx+1,target,candidates,path,ans);
+
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>sol;
         vector<vector<int>>ans;
-        int idx;
-        
-        helper(0,target,sol,ans,candidates);
-        set<vector<int>>result;
+        vector<int>path;
+        helper(0,target,candidates,path,ans);
+        set<vector<int>>st;
         for(int i=0;i<ans.size();i++){
-            result.insert(ans[i]);
+            st.insert(ans[i]);
         }
-        vector<vector<int>>output;
-        for(auto it:result){
-            output.push_back(it);
+        vector<vector<int>>res;
+        for(auto it:st){
+            res.push_back(it);
         }
-        return output;
+        return res;
     }
 };
