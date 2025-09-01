c879ec24b1bbbd9676974c515076d7004accfdaf
Ayush Monga <121785988+AyushMonga@users.noreply.github.com>
2025-09-01 16:39:29 +0530

    Time: 0 ms (100%), Space: 16.9 MB (94.31%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -24,12 +24,13 @@ public:
             for(int i=0;i<size;i++){
                 TreeNode* node=q.front();
                 q.pop();
-                level.push_back(node->val);
                 if(node->left!=nullptr)q.push(node->left);
                 if(node->right!=nullptr)q.push(node->right);
+                level.push_back(node->val);
             }
             ans.push_back(level);
         }
         return ans;
+
     }
 };
