d36a75dbe49bc68f480c6ed0140c09652b788f97
Ayush Monga <121785988+AyushMonga@users.noreply.github.com>
2025-09-08 23:18:19 +0530

    Time: 0 ms (100%), Space: 14.6 MB (57.65%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.cpp
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n=rooms.size();
-        vector<int>visited(n,0);
-        stack<int>st;
-        st.push(0);
-        visited[0]=1;
-        while(!st.empty()){
-            int u=st.top();
-            st.pop();
-            for(int i=0;i<rooms[u].size();i++){
-                if(visited[rooms[u][i]]==0){
-                    visited[rooms[u][i]]=1;
-                    st.push(rooms[u][i]);
+        vector<int> vis(n);
+        vis[0]=1;
+        queue<int>q;
+        q.push(0);
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it:rooms[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push(it);
                 }
             }
         }
-        for(auto it:visited){
-            if(it==0){
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){
                 return false;
             }
         }
