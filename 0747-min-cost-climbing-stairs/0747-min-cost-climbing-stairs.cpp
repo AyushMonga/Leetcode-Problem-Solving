27fe382990916858deb9bb990b03256fd8de101a
Ayush Monga <121785988+AyushMonga@users.noreply.github.com>
2025-09-10 21:44:33 +0530

    Time: 0 ms (100%), Space: 17.7 MB (31.02%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    int helper(int idx, vector<int>&cost,vector<int>&dp){
-        if(idx>=cost.size()){
-            return 0;
-        }
-        if(dp[idx]!=-1)return dp[idx];
-        int ones=helper(idx+1,cost,dp)+cost[idx];
-        int twos=helper(idx+2,cost,dp)+cost[idx];
-        return dp[idx]=min(ones,twos);
-    }
+    // int helper(int idx, vector<int>&cost,vector<int>&dp){
+    //     if(idx>=cost.size()){
+    //         return 0;
+    //     }
+    //     if(dp[idx]!=-1)return dp[idx];
+    //     int ones=helper(idx+1,cost,dp)+cost[idx];
+    //     int twos=helper(idx+2,cost,dp)+cost[idx];
+    //     return dp[idx]=min(ones,twos);
+    // }
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int>dp(n+1,-1);
-        int one=helper(0,cost,dp);
-        int two=helper(1,cost,dp);
-        return min(one,two);
+        vector<int>dp(n+1);
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2;i<=n;i++){
+            int one=dp[i-1]+cost[i-1];
+            int two=dp[i-2]+cost[i-2];
+            dp[i]=min(one,two);
+        }
+        return dp[n];
     }
 };
