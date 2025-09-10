91c4f469430fae8e51919d4f7309d906575ba258
Ayush Monga <121785988+AyushMonga@users.noreply.github.com>
2025-09-10 22:02:37 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,39 +1,15 @@
 class Solution {
 public:
-    // int helper(int idx,vector<int>&nums,vector<int>&dp){
-    //     if(idx==0){
-    //         return nums[idx];
-    //     }
-    //     if(idx<0){
-    //         return 0;
-    //     }
-    //     if(dp[idx]==-1){
-    //     int pick=nums[idx]+helper(idx-2,nums,dp);
-    //     int notpick=0+helper(idx-1,nums,dp);
-    //     dp[idx]=max(pick,notpick);
-    //     }
-    //     return dp[idx];
-
-    // }
-    // int rob(vector<int>& nums) {
-    //     int n=nums.size()-1;
-    //     vector<int>dp(nums.size()+1,-1);
-    //     return helper(n,nums,dp);        
-    // }
-
-    int rob(vector<int>&nums){
-        vector<int>dp(nums.size());
-        if(nums.size()==1){
-            return nums[0];
-        }
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size()+1);
+        if(nums.size()==1)return nums[0];
         dp[0]=nums[0];
         dp[1]=max(nums[0],nums[1]);
         for(int i=2;i<nums.size();i++){
-            int a=nums[i]+dp[i-2];
-            int b=dp[i-1];
-            dp[i]=max(a,b);
+            int opt1=dp[i-2]+nums[i];
+            int opt2=dp[i-1];
+            dp[i]=max(opt1,opt2);
         }
         return dp[nums.size()-1];
     }
-
 };
