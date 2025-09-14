6c5b2cf65f08575798f2461a8632f565be4c32c4
Ayush Monga <121785988+AyushMonga@users.noreply.github.com>
2025-09-14 12:36:57 +0530

    Time: 37 ms (68.65%), Space: 62.5 MB (66.73%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void helper(int idx,int&k,vector<int>&nums,vector<int>&comb,vector<vector<int>>&ans){
+        if(comb.size()==k){
+            ans.push_back(comb);
+            return;
+        }
+        if(idx==nums.size()){
+            return;
+        }
+        comb.push_back(nums[idx]);
+        helper(idx+1,k,nums,comb,ans);
+        comb.pop_back();
+        helper(idx+1,k,nums,comb,ans);
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>nums;
+        for(int i=1;i<=n;i++){
+            nums.push_back(i);
+        }
+        vector<int>comb;
+        vector<vector<int>>ans;
+        helper(0,k,nums,comb,ans);
+        return ans;
+    }
+};
