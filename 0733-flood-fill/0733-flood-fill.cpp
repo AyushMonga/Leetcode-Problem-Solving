1b3d7c84a88b320895f6ea3e1f078f483fecf525
Ayush Monga <121785988+AyushMonga@users.noreply.github.com>
2025-09-15 09:33:45 +0530

    Time: 0 ms (100%), Space: 18.3 MB (34.49%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    void dfs(int r,int c,vector<vector<int>>&vis,vector<vector<int>>&image,int color){
+    void helper(vector<vector<int>>&image,int sr,int sc,int color,vector<vector<int>>&vis){
         int n=image.size();
         int m=image[0].size();
-        vis[r][c]=1;
-        int check=image[r][c];
-        image[r][c]=color;
-        int dr[]={-1,1,0,0};
+        int check=image[sr][sc];
+        vis[sr][sc]=1;
+        image[sr][sc]=color;
+        int dr[]={1,-1,0,0};
         int dc[]={0,0,-1,1};
         for(int i=0;i<4;i++){
-            int nrow=r+dr[i];
-            int ncol=c+dc[i];
-            
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
-                if(!vis[nrow][ncol]){
-                if(image[nrow][ncol]==check){
-                    dfs(nrow,ncol,vis,image,color);
+            int nr=sr+dr[i];
+            int nc=sc+dc[i];
+            if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc]){
+                if(image[nr][nc]==check){
+                    helper(image,nr,nc,color,vis);
                 }
-            }
             }
         }
     }
@@ -25,9 +22,7 @@ public:
         int n=image.size();
         int m=image[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-        if(image[sr][sc]!=color){
-            dfs(sr,sc,vis,image,color);
-        }
+        helper(image,sr,sc,color,vis);
         return image;
     }
 };
