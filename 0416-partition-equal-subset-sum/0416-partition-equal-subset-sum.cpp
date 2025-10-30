4bf86c3eec8bf401a6d8500c5803cb460d2240af
Ayush Monga <121785988+AyushMonga@users.noreply.github.com>
2025-10-30 10:51:22 +0530

    Time: 151 ms (35.74%), Space: 17.4 MB (66.63%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    bool helper(int idx,int target,vector<int>&nums,vector<vector<int>>&dp){
-        if(target==0)return true;
-        if(idx==0)return nums[0]==target;
-        if(dp[idx][target]!=-1)return dp[idx][target];
-        bool not_taken=helper(idx-1,target,nums,dp);
-        bool taken=false;
-        if(nums[idx]<=target){
-            taken=helper(idx-1,target-nums[idx],nums,dp);
-        }
-        return dp[idx][target]=taken|not_taken;
-    }
+    // bool helper(int idx,int target,vector<int>&nums,vector<vector<int>>&dp){
+    //     if(target==0)return true;
+    //     if(idx==0)return nums[0]==target;
+    //     if(dp[idx][target]!=-1)return dp[idx][target];
+    //     bool not_taken=helper(idx-1,target,nums,dp);
+    //     bool taken=false;
+    //     if(nums[idx]<=target){
+    //         taken=helper(idx-1,target-nums[idx],nums,dp);
+    //     }
+    //     return dp[idx][target]=taken|not_taken;
+    // }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
-        for(auto it:nums){
-            sum+=it;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
         }
         if(sum%2!=0)return false;
         int target=sum/2;
-        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
-        return helper(n-1,target,nums,dp);
+        vector<vector<bool>>dp(n,vector<bool>(target+1,false));
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
+        }
+        if(nums[0]<=target)dp[0][nums[0]]=true;
+        for(int idx=1;idx<n;idx++){
+            for(int t=1;t<=target;t++){
+                bool not_taken=dp[idx-1][t];
+                bool taken=false;
+                if(nums[idx]<=t){
+                    taken=dp[idx-1][t-nums[idx]];
+                }
+                dp[idx][t]=not_taken||taken;
+            }
+        }
+        return dp[n-1][target];
     }
-
 };
