00eb8648aac1e988da3d83c65ebef1b9aa7d3d43
Ayush Monga <121785988+AyushMonga@users.noreply.github.com>
2026-05-14 00:01:14 +0530

    Time: 0 ms (100%), Space: 53.7 MB (49.7%) - LeetHub

---
+++ 3921-score-validator/3921-score-validator.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> scoreValidator(vector<string>& events) {
+        int score=0;
+        int counter=0;
+        for(int i=0;i<events.size();i++){
+            if(events[i]=="1"){
+                score++;
+            }
+            else if(events[i]=="2"){
+                score+=2;
+            }
+            else if(events[i]=="3"){
+                score+=3;
+            }
+            else if(events[i]=="4"){
+                score+=4;
+            }
+            else if(events[i]=="5"){
+                score+=5;
+            }
+            else if(events[i]=="6"){
+                score+=6;
+            }
+            else if(events[i]=="W"){
+                counter++;
+                if(counter==10)break;
+            }
+            else if(events[i]=="WD"){
+                score++;
+            }
+            else if(events[i]=="NB"){
+                score++;
+            }
+
+        }
+        return {score,counter};
+    }
+};
